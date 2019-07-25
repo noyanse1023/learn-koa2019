a7a76d986ca473ce4d831daf47fdc124f7406ebf
noyanse1023 <872501560@qq.com>
2019-07-25 21:08:46 +0800

    用户鉴权 如果修改的id和登录的不是同一个id 没权限

--- app/routes/user.js
+++ app/routes/user.js
@@ -3,7 +3,7 @@ const { find, findById, create, update, del, login } = require('../controllers/u
 const jsonwebtoken = require('jsonwebtoken')
 const router = new Router({ prefix: '/user' }) // 给路由加前缀
 
-const auth = (ctx, next) => {
+const auth = async (ctx, next) => {
     const { authorization = '' } = ctx.request.header
     const token = authorization.replace('Bearer', '')
     try {
@@ -12,6 +12,14 @@ const auth = (ctx, next) => {
     } catch(err) {
         ctx.throw(401)
     }
+    await next()
+}
+const checkOwner = async (ctx, next) => {
+    if (ctx.params.id !== ctx.state.user._id) {
+        // 如果请求的id 与登录的不是同一个
+        ctx.throw(403, '没权限')
+    }
+    await next()
 }
 
 router.get('/', find)
@@ -20,7 +28,7 @@ router.get('/:id', findById)
 
 router.post('/', create)
 
-router.patch('/:id', update) // patch 只修改部分
+router.patch('/:id', auth, checkOwner, update) // patch 只修改部分
 
 router.delete('/:id', del)
 
